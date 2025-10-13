f9121a2a42cecfa26f0fb9d7a36a35afdefabf7f
Adhi Ganesh <athiganesh273@gmail.com>
2025-10-13 19:17:44 +0530

    changes

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -11,7 +11,7 @@ const Project = () => {
                     <p className="scroll-slide-left">
                     A real-time messaging application built using React, WebSocket, and Express.js that enables seamless internal communication between users. It allows users to send and receive messages instantly without page reloads. The app ensures low-latency data transfer and real-time message synchronization across all connected clients. The interface is responsive and user-friendly, providing a smooth chatting experience within the system.
                     </p>
-                    <button onClick={() => window.open('https://github.com/your-username/your-project', '_blank')} className='view-project'>
+                    <button onClick={() => window.open('https://github.com/Athiganesh29/googlechatclone', '_blank')} className='view-project'>
                       View project
                     </button>
                 </div>
@@ -23,7 +23,7 @@ const Project = () => {
                     <p className="scroll-slide-left">
                     Manual event and registration management was inefficient, lacked real-time tracking, and offered no centralized control for administrators. To address this, I built ZVENT, a full-stack event management system that streamlines event coordination. It enables users to register, log in, and manage events, while admins can create and manage announcements and publications. The platform ensures data security and real-time updates.
                     </p>
-                    <button onClick={() => window.open('https://github.com/your-username/your-project', '_blank')} className='view-project'>
+                    <button onClick={() => window.open('https://github.com/Athiganesh29/student-event-management-v1', '_blank')} className='view-project'>
                       View project
                     </button>
                 </div>
